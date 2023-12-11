103a7914b470c45a1ddee8ad5e168f4a4fc7f2f3
Imad Ahmad <imadahmad97@yahoo.ca>
2023-12-10 18:24:02 -0800

    added app.py

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import os
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
-    return '<h1>Hello, World!</h1>'
+def index():
+    return render_template('upload.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join('path/to/save', filename))
+            return 'File uploaded successfully'
+    return ''
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() == 'pdf'
+
+if __name__ == '__main__':
+    app.run(debug=True)
